bf3e49841816a57b646da5a38c684cfae4b5913f
Ishak ates <ishakfeuer@gmail.com>
2025-10-02 23:08:42 +0200

    change settings to save information correct in the database

--- conduit/settings.py
+++ conduit/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'conduit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'conduit_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'conduit'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),  # <- Service-Name aus docker-compose
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
